2a8b57da3dea199ac29d9c159a8e4d4327f93247
Pete Keen <peter.keen@bugsplat.info>
2013-03-23 23:30:17 -0700

    move strip_entities to mailer

--- app/mailers/article_mailer.rb
+++ app/mailers/article_mailer.rb
@@ -1,10 +1,15 @@
 class ArticleMailer < ActionMailer::Base
   default :from => 'feeds@bugsplat.info'
 
-  helper :application
+  def strip_entities(text)
+    text.gsub(/&nbsp;/, ' ')
+    text.gsub(/&.{0,}?;/, '')
+  end
 
   def send_article(article)
     @article = article
+    @title = strip_entities(article.title).html_safe
+
     mail to:      'pete@bugsplat.info',
          from:    "#{strip_entities(article.feed)} <feeds@bugsplat.info>",
          subject: strip_entities(article.title)
